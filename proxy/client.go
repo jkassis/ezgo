fc320e2ef449c230e828f8c15a9a1501c0f2fa3d
Jeremy Kassis <jkassis@gmail.com>
2021-08-05 22:14:28 -0700

    fix registration

--- proxy/client.go
+++ proxy/client.go
@@ -11,12 +11,9 @@ import (
 
 	"github.com/inconshreveable/muxado"
 	ezenv "github.com/jkassis/ezgo/env"
-	"github.com/libp2p/go-libp2p-core/host"
 	ms "github.com/multiformats/go-multistream"
 	log "github.com/sirupsen/logrus"
 )
-
-var peerService host.Host
 
 var proxyProxeeServiceAdvertisedHost string
 var proxyProxeeServiceAdvertisedPort int64
@@ -67,12 +64,11 @@ func Connect(hostname string, mux *ms.MultistreamMuxer) error {
 				defer req.Close()
 
 				// send the req
-				_, err = rwc.Write([]byte(hostname))
+				_, err = rwc.Write([]byte(hostname + "\n"))
 				if err != nil {
 					log.Errorf("proxy.Client.Connect: %s", err.Error())
 					return
 				}
-				rwc.Close()
 
 				// read the response
 				resp, err := ioutil.ReadAll(rwc)
@@ -83,10 +79,8 @@ func Connect(hostname string, mux *ms.MultistreamMuxer) error {
 
 				// validate response
 				if !bytes.Equal(resp, []byte("OK")) {
-					if err != nil {
-						log.Errorf("proxy.Client.Connect: %s", err.Error())
-						return
-					}
+					log.Errorf("proxy.Client.Connect: expected 'OK' got '%s'", resp)
+					return
 				}
 			}()
 
