1a2d5d91f921498560bc12e316d3fe7e1805fb48
Jeremy Kassis <jkassis@gmail.com>
2021-09-22 08:24:52 -0700

    add ping to proxy conn

--- proxy/client.go
+++ proxy/client.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"net"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/inconshreveable/muxado"
@@ -34,6 +35,56 @@ func init() {
 
 // Connect to the proxy
 func Connect(hostname string, mux *ms.MultistreamMuxer) error {
+	var streamMux muxado.Session
+	streamMuxMut := sync.Mutex{}
+
+	// ping forever
+	go func() {
+		ticker := time.NewTicker(30 * time.Second)
+		for {
+			<-ticker.C
+
+			streamMuxMut.Lock()
+			streamMuxL := streamMux
+			streamMuxMut.Unlock()
+
+			if streamMuxL == nil {
+				continue
+			}
+			req, err := streamMuxL.Open()
+			if err != nil {
+				log.Errorf("proxy.Client.Connect.ping: streamMux.Open: %s", err.Error())
+				continue
+			}
+
+			// make a new multistream
+			ms := ms.NewMSSelect(req, "/ping")
+			defer ms.Close()
+			defer req.Close()
+
+			// send the ping req
+			_, err = fmt.Fprintln(ms, `ping`)
+			if err != nil {
+				log.Errorf("proxy.Client.Connect.ping: send: %s", err.Error())
+				continue
+			}
+
+			// read the ping response
+			scanner := bufio.NewScanner(ms)
+			if !scanner.Scan() {
+				log.Errorf("proxy.Client.Connect.ping: could not read response... no input")
+				continue
+			}
+			resp := scanner.Text()
+
+			// validate it
+			if resp != "pong" {
+				log.Errorf("proxy.Client.Connect.ping: expected 'pong' got '%s'", resp)
+				continue
+			}
+		}
+	}()
+
 	// dial forever with 10 second intervals
 	for {
 		log.Infof("Dialing %s", fmt.Sprintf("%s:%d", proxyProxeeServiceAdvertisedHost, proxyProxeeServiceAdvertisedPort))
@@ -43,51 +94,57 @@ func Connect(hostname string, mux *ms.MultistreamMuxer) error {
 			time.Sleep(10*time.Second + time.Duration(rand.Int63n(int64(2*time.Second))))
 			continue
 		}
-		// mplex := multiplex.NewMultiplex(conn, true)
-		mplex := muxado.Client(conn, new(muxado.Config))
 
-		// Dial the proxy
+		// register self and handle requests
 		func() {
-			defer mplex.Close()
-			defer conn.Close()
+			streamMuxMut.Lock()
+			streamMux = muxado.Client(conn, new(muxado.Config))
+			streamMuxMut.Unlock()
 
-			// register ourselves
-			func() {
-				req, err := mplex.Open()
-				if err != nil {
-					log.Errorf("proxy.Client.Connect: mplex.Open: %s", err.Error())
-					return
-				}
-				rwc := ms.NewMSSelect(req, "/register")
-				defer rwc.Close()
-				defer req.Close()
-
-				// send the req
-				log.Infof("proxy.Client.Connect: register: sending hostname: %s", hostname)
-				_, err = fmt.Fprintln(rwc, hostname)
-				if err != nil {
-					log.Errorf("proxy.Client.Connect: %s", err.Error())
-					return
-				}
-
-				// read the response
-				scanner := bufio.NewScanner(rwc)
-				if !scanner.Scan() {
-					log.Errorf("proxy.Client.Connect: register: could not read response... no input")
-					return
-				}
-				resp := scanner.Text()
-
-				// validate it
-				if resp != "OK" {
-					log.Errorf("proxy.Client.Connect: register: expected 'OK' got '%s'", resp)
-					return
-				}
-				log.Info("proxy.Client.Connect: register: success")
+			defer func() {
+				streamMuxMut.Lock()
+				streamMux.Close()
+				streamMux = nil
+				streamMuxMut.Unlock()
+				conn.Close()
 			}()
 
+			// register ourselves
+			req, err := streamMux.Open()
+			if err != nil {
+				log.Errorf("proxy.Client.Connect: streamMux.Open: %s", err.Error())
+				return
+			}
+			ms := ms.NewMSSelect(req, "/register")
+			defer ms.Close()
+			defer req.Close()
+
+			// send the register req
+			log.Infof("proxy.Client.Connect: register: sending hostname: %s", hostname)
+			_, err = fmt.Fprintln(ms, hostname)
+			if err != nil {
+				log.Errorf("proxy.Client.Connect: %s", err.Error())
+				return
+			}
+
+			// read the register response
+			scanner := bufio.NewScanner(ms)
+			if !scanner.Scan() {
+				log.Errorf("proxy.Client.Connect: register: could not read response... no input")
+				return
+			}
+			resp := scanner.Text()
+
+			// validate it
+			if resp != "OK" {
+				log.Errorf("proxy.Client.Connect: register: expected 'OK' got '%s'", resp)
+				return
+			}
+			log.Info("proxy.Client.Connect: register: success")
+
+			// listen for requests and handle them
 			for {
-				stream, err := mplex.Accept()
+				stream, err := streamMux.Accept()
 				if err != nil {
 					log.Errorf("proxy.Client: mplex.Accept err: %s", err.Error())
 					return
